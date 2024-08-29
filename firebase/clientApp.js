ea7be6242987ce2c53c5766a53fa325e9584f16e
Vinh Nguyen <ntvinhgv@gmail.com>
2024-08-28 22:01:28 -0400

    Clean code and add api_key to secret

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -1,58 +1,14 @@
-// // firebase/clientApp.js
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA2a7UquW_E1aOsOV4_PbCPRJGYfcEZJvg",
-//   authDomain: "rate-my-prof-89395.firebaseapp.com",
-//   projectId: "rate-my-prof-89395",
-//   storageBucket: "rate-my-prof-89395.appspot.com",
-//   messagingSenderId: "220658571409",
-//   appId: "1:220658571409:web:87dc4ab3228dde2bb80730",
-//   measurementId: "G-MGVLJ31E23"
-// };
-
-// async function addRecommendation(teacherName, recommendation, rating) {
-//     try {
-//       const docRef = await addDoc(collection(db, "recommendations"), {
-//         teacherName: teacherName,
-//         recommendation: recommendation,
-//         rating: rating,
-//         timestamp: new Date()
-//       });
-//       console.log("Document written with ID: ", docRef.id);
-//     } catch (e) {
-//       console.error("Error adding document: ", e);
-//     }
-//   }
-
-//   async function getRecommendations() {
-//     const recommendationsArray = [];
-//     const querySnapshot = await getDocs(collection(db, "recommendations"));
-//     querySnapshot.forEach((doc) => {
-//       recommendationsArray.push({ id: doc.id, ...doc.data() });
-//     });
-//     return recommendationsArray;
-//   }
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-
-// export { db };
-
-// firebase/clientApp.js
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2a7UquW_E1aOsOV4_PbCPRJGYfcEZJvg",
-  authDomain: "rate-my-prof-89395.firebaseapp.com",
-  projectId: "rate-my-prof-89395",
-  storageBucket: "rate-my-prof-89395.appspot.com",
-  messagingSenderId: "220658571409",
-  appId: "1:220658571409:web:87dc4ab3228dde2bb80730",
-  measurementId: "G-MGVLJ31E23"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
